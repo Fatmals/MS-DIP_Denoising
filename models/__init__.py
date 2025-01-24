418e2baecf531db5cc66b4a01e534552113c3fad
Fatmals <79986761+Fatmals@users.noreply.github.com>
2025-01-24 16:36:28 +0000

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -2,13 +2,12 @@ from .skip import skip
 from .texture_nets import get_texture_nets
 from .resnet import ResNet
 from .unet import UNet
-from .multi_scale_dip import MultiScaleNetwork  # Import the MultiScaleNetwork
 
 import torch.nn as nn
 
 def get_net(input_depth, NET_TYPE, pad, upsample_mode, n_channels=3, act_fun='LeakyReLU', skip_n33d=128, skip_n33u=128, skip_n11=4, num_scales=5, downsample_mode='stride'):
     if NET_TYPE == 'ResNet':
-        # ResNet implementation
+        # TODO
         net = ResNet(input_depth, 3, 10, 16, 1, nn.BatchNorm2d, False)
     elif NET_TYPE == 'skip':
         net = skip(input_depth, n_channels, num_channels_down = [skip_n33d]*num_scales if isinstance(skip_n33d, int) else skip_n33d,
@@ -16,19 +15,18 @@ def get_net(input_depth, NET_TYPE, pad, upsample_mode, n_channels=3, act_fun='Le
                                             num_channels_skip = [skip_n11]*num_scales if isinstance(skip_n11, int) else skip_n11, 
                                             upsample_mode=upsample_mode, downsample_mode=downsample_mode,
                                             need_sigmoid=True, need_bias=True, pad=pad, act_fun=act_fun)
+
     elif NET_TYPE == 'texture_nets':
-        net = get_texture_nets(inp=input_depth, ratios = [32, 16, 8, 4, 2, 1], fill_noise=False, pad=pad)
-    elif NET_TYPE == 'UNet':
-        net = UNet(num_input_channels=input_depth, num_output_channels=n_channels, 
+        net = get_texture_nets(inp=input_depth, ratios = [32, 16, 8, 4, 2, 1], fill_noise=False,pad=pad)
+
+    elif NET_TYPE =='UNet':
+        net = UNet(num_input_channels=input_depth, num_output_channels=3, 
                    feature_scale=4, more_layers=0, concat_x=False,
                    upsample_mode=upsample_mode, pad=pad, norm_layer=nn.BatchNorm2d, need_sigmoid=True, need_bias=True)
-    elif NET_TYPE == 'MultiScale':
-        net = MultiScaleNetwork(num_input_channels=input_depth, num_output_channels=n_channels, 
-                                num_filters=[64, 128, 256], need_sigmoid=True, need_bias=True)
     elif NET_TYPE == 'identity':
         assert input_depth == 3
         net = nn.Sequential()
     else:
-        assert False, f"Unsupported NET_TYPE: {NET_TYPE}"
+        assert False
 
     return net
