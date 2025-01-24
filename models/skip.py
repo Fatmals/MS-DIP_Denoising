ce7042d8daffce831cfb894ee10e4f8253fb36d5
Fatmals <79986761+Fatmals@users.noreply.github.com>
2025-01-24 16:40:23 +0000

    Update skip.py

--- models/skip.py
+++ models/skip.py
@@ -2,48 +2,32 @@ import torch
 import torch.nn as nn
 from .common import *
 
-def skip(
+def enhanced_multiscale_skip(
         num_input_channels=2, num_output_channels=3, 
-        num_channels_down=[16, 32, 64, 128, 128], num_channels_up=[16, 32, 64, 128, 128], num_channels_skip=[4, 4, 4, 4, 4], 
+        num_channels_down=[16, 32, 64, 128, 128], num_channels_up=[16, 32, 64, 128, 128], 
+        num_channels_skip=[4, 4, 4, 4, 4], 
         filter_size_down=3, filter_size_up=3, filter_skip_size=1,
         need_sigmoid=True, need_bias=True, 
         pad='zero', upsample_mode='nearest', downsample_mode='stride', act_fun='LeakyReLU', 
         need1x1_up=True):
-    """Assembles encoder-decoder with skip connections.
-
-    Arguments:
-        act_fun: Either string 'LeakyReLU|Swish|ELU|none' or module (e.g. nn.ReLU)
-        pad (string): zero|reflection (default: 'zero')
-        upsample_mode (string): 'nearest|bilinear' (default: 'nearest')
-        downsample_mode (string): 'stride|avg|max|lanczos2' (default: 'stride')
-
-    """
     assert len(num_channels_down) == len(num_channels_up) == len(num_channels_skip)
 
     n_scales = len(num_channels_down) 
 
-    if not (isinstance(upsample_mode, list) or isinstance(upsample_mode, tuple)) :
-        upsample_mode   = [upsample_mode]*n_scales
+    # Ensuring list input for modes and filter sizes
+    upsample_mode = [upsample_mode] * n_scales if not isinstance(upsample_mode, list) else upsample_mode
+    downsample_mode = [downsample_mode] * n_scales if not isinstance(downsample_mode, list) else downsample_mode
+    filter_size_down = [filter_size_down] * n_scales if not isinstance(filter_size_down, list) else filter_size_down
+    filter_size_up = [filter_size_up] * n_scales if not isinstance(filter_size_up, list) else filter_size_up
 
-    if not (isinstance(downsample_mode, list)or isinstance(downsample_mode, tuple)):
-        downsample_mode   = [downsample_mode]*n_scales
-    
-    if not (isinstance(filter_size_down, list) or isinstance(filter_size_down, tuple)) :
-        filter_size_down   = [filter_size_down]*n_scales
-
-    if not (isinstance(filter_size_up, list) or isinstance(filter_size_up, tuple)) :
-        filter_size_up   = [filter_size_up]*n_scales
-
-    last_scale = n_scales - 1 
-
-    cur_depth = None
+    # Multi-scale output layers
+    output_layers = []
 
     model = nn.Sequential()
     model_tmp = model
 
     input_depth = num_input_channels
-    for i in range(len(num_channels_down)):
-
+    for i in range(n_scales):
         deeper = nn.Sequential()
         skip = nn.Sequential()
 
@@ -58,8 +42,6 @@ def skip(
             skip.add(conv(input_depth, num_channels_skip[i], filter_skip_size, bias=need_bias, pad=pad))
             skip.add(bn(num_channels_skip[i]))
             skip.add(act(act_fun))
-            
-        # skip.add(Concat(2, GenNoise(nums_noise[i]), skip_part))
 
         deeper.add(conv(input_depth, num_channels_down[i], filter_size_down[i], 2, bias=need_bias, pad=pad, downsample_mode=downsample_mode[i]))
         deeper.add(bn(num_channels_down[i]))
@@ -71,29 +53,31 @@ def skip(
 
         deeper_main = nn.Sequential()
 
-        if i == len(num_channels_down) - 1:
-            # The deepest
+        if i == n_scales - 1:
+            # The deepest layer
             k = num_channels_down[i]
         else:
             deeper.add(deeper_main)
             k = num_channels_up[i + 1]
 
         deeper.add(nn.Upsample(scale_factor=2, mode=upsample_mode[i]))
-
         model_tmp.add(conv(num_channels_skip[i] + k, num_channels_up[i], filter_size_up[i], 1, bias=need_bias, pad=pad))
         model_tmp.add(bn(num_channels_up[i]))
         model_tmp.add(act(act_fun))
-
 
         if need1x1_up:
             model_tmp.add(conv(num_channels_up[i], num_channels_up[i], 1, bias=need_bias, pad=pad))
             model_tmp.add(bn(num_channels_up[i]))
             model_tmp.add(act(act_fun))
 
+        # Adding multi-scale outputs
+        output_layers.append(conv(num_channels_up[i], num_output_channels, 1, bias=need_bias, pad=pad))
+
         input_depth = num_channels_down[i]
         model_tmp = deeper_main
 
-    model.add(conv(num_channels_up[0], num_output_channels, 1, bias=need_bias, pad=pad))
+    model.add(nn.ModuleList(output_layers))
+
     if need_sigmoid:
         model.add(nn.Sigmoid())
 
