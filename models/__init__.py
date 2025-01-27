5ec8ee9a4134ef7877cc6dae156efeae5c1c9bac
Fatmals <79986761+Fatmals@users.noreply.github.com>
2025-01-27 15:08:19 +0000

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -21,11 +21,11 @@ def get_net(input_depth, NET_TYPE, pad, upsample_mode='bilinear', norm_layer=Non
                    num_channels_skip=[skip_n11]*num_scales, 
                    upsample_mode=upsample_mode, need_sigmoid=True, need_bias=True, pad=pad, act_fun=act_fun)
     elif NET_TYPE == 'MultiScaleUNet':
-        net = MultiScaleUNet(num_input_channels=input_depth, num_output_channels=3,
-                             scales=[1, 0.5, 0.25], feature_scale=4, more_layers=0,
-                             concat_x=False, upsample_mode=upsample_mode,
-                             pad=pad, norm_layer=norm_layer,
-                             need_sigmoid=True, need_bias=True)
+        return MultiScaleUNet(num_input_channels=input_depth, num_output_channels=3,
+                              scales=[1, 0.5, 0.25], feature_scale=4, more_layers=0,
+                              concat_x=False, upsample_mode=upsample_mode,
+                              pad=pad, norm_layer=norm_layer if norm_layer is not None else nn.BatchNorm2d,
+                              need_sigmoid=True, need_bias=True)
     elif NET_TYPE == 'texture_nets':
         net = get_texture_nets(inp=input_depth, ratios=[32, 16, 8, 4, 2, 1], fill_noise=False, pad=pad)
     elif NET_TYPE == 'identity':
