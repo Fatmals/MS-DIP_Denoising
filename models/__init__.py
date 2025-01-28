4ed2ddf06e909d330703002f80e1ac610f23e673
Fatmals <79986761+Fatmals@users.noreply.github.com>
2025-01-28 20:56:47 +0000

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -7,7 +7,7 @@ import torch.nn as nn
 
 def get_net(input_depth, NET_TYPE, pad, upsample_mode, n_channels=3, act_fun='LeakyReLU',
             skip_n33d=128, skip_n33u=128, skip_n11=4, num_scales=5, downsample_mode='stride',
-            scales=None, norm_layer=nn.BatchNorm2d):
+            scales=[2.5, 2, 1.5], norm_layer=nn.BatchNorm2d):
     if NET_TYPE == 'ResNet':
         net = ResNet(input_depth, 3, 10, 16, 1, nn.BatchNorm2d, False)
     
