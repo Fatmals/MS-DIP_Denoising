f6c08e0741dbd1c9e0d2f073c6c5a5d744e00b55
Fatmals <79986761+Fatmals@users.noreply.github.com>
2025-01-28 14:50:59 +0000

    Update denoising_utils.py

--- utils/denoising_utils.py
+++ utils/denoising_utils.py
@@ -2,14 +2,26 @@ import os
 from .common_utils import *
 
 
-        
 from torchvision.transforms.functional import resize
 
 def get_multi_scale_noisy_images(img_np, scales, sigma):
+    """
+    Adds Gaussian noise to an image at multiple scales.
+    
+    Args:
+        img_np: image, np.array with values from 0 to 1
+        scales: list of scales (e.g., [1, 0.5, 0.25]) to generate multi-scale versions
+        sigma: std of the noise
+    
+    Returns:
+        img_noisy_pils: list of PIL images with noise at each scale
+        img_noisy_nps: list of NumPy arrays with noise at each scale
+    """
     img_noisy_pils = []
     img_noisy_nps = []
+    
     for scale in scales:
-        # Downsample the image
+        # Downsample image
         h, w = img_np.shape[1], img_np.shape[2]
         scaled_img = resize(np_to_pil(img_np), size=(int(h * scale), int(w * scale)))
         scaled_img_np = pil_to_np(scaled_img)
@@ -22,4 +34,3 @@ def get_multi_scale_noisy_images(img_np, scales, sigma):
         img_noisy_nps.append(noisy_img_np)
     
     return img_noisy_pils, img_noisy_nps
-
