51a39ebdfaf2f1d10cfb7300a8de4d9a368e9f00
Fatmals <79986761+Fatmals@users.noreply.github.com>
2025-01-30 15:44:45 +0000

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -5,30 +5,22 @@ from .unet import UNet
 
 import torch.nn as nn
 
-def get_net(input_depth, NET_TYPE, pad, upsample_mode, n_channels=3, act_fun='LeakyReLU', skip_n33d=128, skip_n33u=128, skip_n11=4, num_scales=5, downsample_mode='stride'):
-    if NET_TYPE == 'ResNet':
-        # TODO
+def get_net(input_depth, NET_TYPE, pad, upsample_mode, n_channels=3, act_fun='LeakyReLU', skip_n33d=128, skip_n33u=128, skip_n11=4, scales=[1.0], num_scales=5, downsample_mode='stride'):
+    """
+    Constructs a multi-scale version of the network.
+    Initializes multiple networks (one per scale) and fuses results.
+    """
+    from skip import multi_scale_skip  # Use the updated skip model
+
+    if NET_TYPE == 'skip':
+        net = multi_scale_skip(input_depth, n_channels, scales, pad)
+    elif NET_TYPE == 'ResNet':
         net = ResNet(input_depth, 3, 10, 16, 1, nn.BatchNorm2d, False)
-    elif NET_TYPE == 'skip':
-        net = skip(input_depth, n_channels, num_channels_down = [skip_n33d]*num_scales if isinstance(skip_n33d, int) else skip_n33d,
-                                            num_channels_up =   [skip_n33u]*num_scales if isinstance(skip_n33u, int) else skip_n33u,
-                                            num_channels_skip = [skip_n11]*num_scales if isinstance(skip_n11, int) else skip_n11, 
-                                            upsample_mode=upsample_mode, downsample_mode=downsample_mode,
-                                            need_sigmoid=True, need_bias=True, pad=pad, act_fun=act_fun)
-
     elif NET_TYPE == 'texture_nets':
-        net = get_texture_nets(inp=input_depth, ratios = [32, 16, 8, 4, 2, 1], fill_noise=False,pad=pad)
-
-    elif NET_TYPE =='UNet':
-        net = UNet(num_input_channels=input_depth, num_output_channels=3, 
-                   feature_scale=4, more_layers=0, concat_x=False,
-                   upsample_mode=upsample_mode, pad=pad, norm_layer=nn.BatchNorm2d, need_sigmoid=True, need_bias=True)
-    elif NET_TYPE == 'identity':
-        assert input_depth == 3
-        net = nn.Sequential()
+        net = get_texture_nets(inp=input_depth, num_output_channels=n_channels, pad=pad, act_fun=act_fun)
     else:
-        assert False
-
+        raise ValueError(f"Unsupported NET_TYPE: {NET_TYPE}")
+    
     return net
 
 
