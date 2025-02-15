680332bfdcefd0b6229b1ec89e642b49496df820
Fatmals <79986761+Fatmals@users.noreply.github.com>
2025-02-15 14:01:11 +0000

    Update common.py

--- models/common.py
+++ models/common.py
@@ -73,7 +73,7 @@ class Swish(nn.Module):
         return x * self.s(x)
 
 
-def act(act_fun = 'ReLU'):
+def act(act_fun = 'LeakyReLU'):
     '''
         Either string defining an activation function or module (e.g. nn.ReLU)
     '''
