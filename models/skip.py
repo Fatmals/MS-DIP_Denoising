d65571caddd66eb86df1c7ef2e3deacbac9b8331
Fatmals <79986761+Fatmals@users.noreply.github.com>
2025-04-17 19:07:47 +0100

    Update skip.py

--- models/skip.py
+++ models/skip.py
@@ -113,7 +113,7 @@ def multi_scale_skip(input_depth, num_output_channels, scales, pad='reflection')
             input_depth, num_output_channels,
             num_channels_down=[8, 16, 32, 64, 64], 
             num_channels_up=[8, 16, 32, 64, 64],
-            num_channels_skip=[4, 4, 4, 4, 4],
+            num_channels_skip=[0, 0, 4, 4, 4],
             upsample_mode='bilinear',
             need_sigmoid=True, need_bias=True, pad=pad, act_fun='LeakyReLU'
         )
